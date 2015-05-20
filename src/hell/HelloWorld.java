035fc57352c0016605f18b3635e97fc39354f1d6
Masur Ho <mushyho@gmail.com>
2015-05-19 17:57:12 -0700

    Initial commit

---
+++ src/hell/HelloWorld.java
@@ -0,0 +1,16 @@
+package hell;
+
+/**
+ * Created by mho on 2015-05-19.
+ */
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
